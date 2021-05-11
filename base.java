e9633ab9c18308fe50e96f4c2eff1d4bc11a32c1
Stepan Shcherbakov <stepan_shcherbakov@epam.com>
2021-05-11 17:53:20 +0300

    Highly understandable message
    Add division
    
    Add multiplication
    
    Add correct sqrt implementation

--- base.java
+++ base.java
@@ -1,4 +1,8 @@
 package jmp07week; 
 public class Base { 
   public int add(int a, int b) {return a + b;} 
+  public int sub(int a, int b) {return a - b;} 
+  public int div(int a, int b) {return a / b;} 
+  public int mult(int a, int b) {return a * b;} 
+  public int sqrt(int a) {return Math.sqrt(a);} 
 } 
