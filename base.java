4e5fea28eef7557e38a75db8f63490a07182309f
Stepan Shcherbakov <stepan_shcherbakov@epam.com>
2021-05-11 19:50:30 +0300

    Add with 2 and 3 parameters

--- base.java
+++ base.java
@@ -1,7 +1,11 @@
 package jmp07week; 
 public class Base { 
   public int add(int a, int b) {
-    System.out("This is add method");
+    System.out("This is add method for 2 params");
     return a + b;
+  } 
+  public int add(int a, int b, int c) {
+    System.out("This is add method for 3 params");
+    return a + b + c;
   } 
 } 
