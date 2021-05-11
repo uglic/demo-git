db82b48c1b43468e504c108623f64e69bdcb99d1
Stepan Shcherbakov <stepan_shcherbakov@epam.com>
2021-05-11 20:04:44 +0300

    Update 2 param add by second logger

--- base.java
+++ base.java
@@ -2,6 +2,7 @@ package jmp07week;
 public class Base { 
   public int add(int a, int b) {
     System.out("This is add method for 2 params");
+    System.out("This is add method");
     return a + b;
   } 
   public int add(int a, int b, int c) {
